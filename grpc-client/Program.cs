6642d98efac71fed1a6e9d2885b3e051164c502a
Jabari Dawkins <jabaridawkins@gmail.com>
2020-03-15 16:32:20 -0400

    Spin up example grpc client and server

---
+++ grpc-client/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+
+using grpc_server;
+
+namespace grpc_client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.Write("Please enter your name: ");
+            var name = Console.ReadLine();
+            Console.Write("Please enter your birthday: ");
+            var birthday = Console.ReadLine();
+            var req = new PersonInfo() {
+                Name = name,
+                Birthday = birthday
+            };
+
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new BirthdayGreeter.BirthdayGreeterClient(channel);
+            var reply = await client.GreetWithBirthdayMessageAsync(req);
+
+            Console.WriteLine(reply.Message);
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+            await channel.ShutdownAsync();
+        }
+    }
+}
